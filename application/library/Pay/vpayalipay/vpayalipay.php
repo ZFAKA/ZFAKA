64a99dd9926ebb7b11fae60cf5f4a0003b9989b8
ZFAKA <turby3316@gmail.com>
2025-10-03 23:20:12 +0800

    修复issue中提到的windows下支付接口请求失败的问题

--- application/library/Pay/vpayalipay/vpayalipay.php
+++ application/library/Pay/vpayalipay/vpayalipay.php
@@ -115,24 +115,5 @@ class vpayalipay
             }
         }
     }
-
-	
-	
-	private function _curlPost($url,$params){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,$url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_TIMEOUT,300); //设置超时
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // https请求 不验证证书和hosts
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		return $result;	
-	}
-
-
-	
 	
 }
